660427093b0acb9620e14bf3f31d589b4649e88f
visalpii <xtrzne@gmail.com>
2018-10-25 14:07:41 -0400

    first commit

---
+++ precache-manifest.23c5c4a1fc7f775f784ef02286ad5286.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "60491363bc8abaf05623",
+    "url": "/static/css/main.024dcb72.chunk.css"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "a12dbf181fcf11051150",
+    "url": "/static/js/1.a12dbf18.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "60491363bc8abaf05623",
+    "url": "/static/js/main.60491363.chunk.js"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "a12dbf181fcf11051150",
+    "url": "/static/css/1.2233b786.chunk.css"
+  },
+  {
+    "revision": "fd2c26d6e367be2cfa7ef738369abb57",
+    "url": "/index.html"
+  }
+];
